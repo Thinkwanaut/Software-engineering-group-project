2d8abfdce42b3764991264be6410549d943fc1be
Xphalt <41804445+Xphalt@users.noreply.github.com>
2022-01-24 18:30:26 +0000

    Added project files

---
+++ GameEngine/Main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <SDL.h>
+#include <SDL_ttf.h>
+#include <SDL_mixer.h>
+#include <SDL_image.h>
+
+int main(int argc, char* args[])
+{
+    std::cout << "Hello World!" << std::endl;
+
+    return 0;
+}
